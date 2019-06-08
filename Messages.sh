6839ebf3b7b01419350027b3dab4c64620e8612c
Jonah Brüchert <jbb.prv@gmx.de>
2019-06-08 15:38:30 +0200

    Messages.sh: update shell syntax

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/plasma-camera.pot
+$XGETTEXT $(find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/plasma-camera.pot
