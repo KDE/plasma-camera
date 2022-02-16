1c638ba90c3f2e8cff89ac6877f9452306a48214
Devin Lin <espidev@gmail.com>
2022-02-16 09:45:37 -0500

    Make REUSE compliant, and also add reuse check to ci

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2018 Jonah Brüchert <jbb@kaidan.im>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
 $XGETTEXT $(find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/plasma-camera.pot
